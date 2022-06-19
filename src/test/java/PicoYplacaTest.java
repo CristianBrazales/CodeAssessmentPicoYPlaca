fe2ba90343b9e7a84efd0bff55f567dda1b23f28
Cristian <cristianbrazalesmolina@gmail.com>
2022-06-18 20:59:54 -0400

    Project structure and test added

---
+++ src/test/java/PicoYplacaTest.java
@@ -0,0 +1,7 @@
+import junit.framework.TestCase;
+
+public class PicoYplacaTest extends TestCase {
+    public void testPicoYplaca() {
+        assertFalse("should fail test", (1 == 1));
+    }
+}
---
+++ src/test/java/VehicleTest.java
@@ -0,0 +1,8 @@
+import junit.framework.TestCase;
+
+public class VehicleTest extends TestCase {
+
+    public void testCanDriveAt() {
+        assertFalse("should fail test", (1 == 1));
+    }
+}
