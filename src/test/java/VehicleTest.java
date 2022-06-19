a5241bf29247a866054fbbc0c1c611be20ba3f1c
Cristian <cristianbrazalesmolina@gmail.com>
2022-06-18 21:28:50 -0400

    Test cases added

--- src/test/java/VehicleTest.java
+++ src/test/java/VehicleTest.java
@@ -1,8 +1,32 @@
 import junit.framework.TestCase;
+import org.junit.Test;
+
+import static org.junit.Assert.assertThrows;
 
 public class VehicleTest extends TestCase {
+    private int[][] arr = {{5}, {1}, {1}, {2}, {2}, {3}, {3}, {4}, {4}, {5}};
+    private String[][] hourPeriods = {{"7:00", "9:00"}, {"16:00", "19:30"}};
+    private PicoYplaca restrictions = new PicoYplaca(arr, hourPeriods);
 
-    public void testCanDriveAt() {
-        assertFalse("should fail test", (1 == 0));
+    @Test
+    public void testForNonValidInputsForCarCreation() {
+        // Non Numeric
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            Vehicle car1 = new Vehicle("AVVAVVV", restrictions);
+        });
+        // invalid length
+        Exception exception2 = assertThrows(IllegalArgumentException.class, () -> {
+            Vehicle car1 = new Vehicle("AAAAAAAAAAAA", restrictions);
+        });
+        // empty parameters
+        Exception exception3 = assertThrows(IllegalArgumentException.class, () -> {
+            Vehicle car1 = new Vehicle("", restrictions);
+        });
+        // null parameter
+        Exception exception4 = assertThrows(IllegalArgumentException.class, () -> {
+            Vehicle car1 = new Vehicle(null, restrictions);
+        });
     }
+
+
 }
